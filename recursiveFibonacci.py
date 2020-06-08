f32822c8db306fa41413f23706578b8cbb1db03f
halljc76 <59719553+halljc76@users.noreply.github.com>
2020-06-08 11:49:25 -0400

    Update recursiveFibonacci.py

--- recursiveFibonacci.py
+++ recursiveFibonacci.py
@@ -3,6 +3,9 @@ import time
 
 @lru_cache(maxsize = 1000)
 def recursiveFibonacci(n):
+    """Recursive implementation of Fibonacci Sequence.
+       Function uses the "1,1,2..." version of the sequence."""
+    
     if type(n) != int:
         raise TypeError("Parameter must be a positive integer.")
     elif n < 1:
@@ -12,6 +15,7 @@ def recursiveFibonacci(n):
         return 1 
     return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
 
+# Prints out first n-terms of Fibonacci Sequence.
 start = time.time()
 for i in range(1,501):
     print(str(i) + ": ", recursiveFibonacci(i))
